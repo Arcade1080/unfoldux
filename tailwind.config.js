952ce1a02da8d9182a5c8402b83963b2bcfd13f5
Julian Scheele <>
2021-09-30 13:18:59 +0200

    config

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,20 +3,7 @@ module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {
-      container: {
-        center: true,
-        padding: '1rem',
-      },
-      fontFamily: {
-        sans: ['Inter', ...defaultTheme.fontFamily.sans],
-      },
-      colors: {
-        unfoldgray: {
-          950: '#1a337b',
-        },
-      },
-    },
+    extend: {},
   },
   variants: {
     extend: {},
